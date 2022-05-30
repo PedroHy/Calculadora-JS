159a39122d3eee0309aa046628bdf3967719b8a3
Pedro Henrique Ribeiro de Azevedo <62411379+PedroHy@users.noreply.github.com>
2022-05-30 10:53:59 -0300

    Add files via upload

---
+++ capturar-numeros.js
@@ -0,0 +1,71 @@
+let text = []
+
+function capturarZero(){
+    document.querySelector("textarea#numeros").innerHTML += "0"
+    text.push(0)
+}
+function capturarOne(){
+    document.querySelector("textarea#numeros").innerHTML += "1"
+    text.push(1)
+}
+function capturarTwo(){
+    document.querySelector("textarea#numeros").innerHTML += "2"
+    text.push(2)
+}
+function capturarTree(){
+    document.querySelector("textarea#numeros").innerHTML += "3"
+    text.push(3)
+}
+function capturarFour(){
+    document.querySelector("textarea#numeros").innerHTML += "4"
+    text.push(4)
+}
+function capturarFive(){
+    document.querySelector("textarea#numeros").innerHTML += "5"
+    text.push(5)
+}
+function capturarSix(){
+    document.querySelector("textarea#numeros").innerHTML += "6"
+    text.push(6)
+}
+function capturarSeven(){
+    document.querySelector("textarea#numeros").innerHTML += "7"
+    text.push(7)
+}
+function capturarEight(){
+    document.querySelector("textarea#numeros").innerHTML += "8"
+    text.push(8)
+}
+function capturarNine(){
+    document.querySelector("textarea#numeros").innerHTML += "9"
+    text.push(9)
+}
+function virgula(){
+    document.querySelector("textarea#numeros").innerHTML += ","
+    text.push('.')
+}
+function ponto(){
+    document.querySelector("textarea#numeros").innerHTML += "."
+    text.push('.')
+}
+function soma(){
+    document.querySelector("textarea#numeros").innerHTML += "+"
+    text.push('+')
+}
+function subtracao(){
+    document.querySelector("textarea#numeros").innerHTML += "-"
+    text.push('-')
+}
+function multiplicacao(){
+    document.querySelector("textarea#numeros").innerHTML += "x"
+    text.push('x')
+}
+function divisao(){
+    document.querySelector("textarea#numeros").innerHTML += "/"
+    text.push('/')
+}
+function percent(){
+    document.querySelector("textarea#numeros").innerHTML += "%"
+    text.push('%')
+}
+
